b1f7eab62dcddc0a0676a7499eef3ea7ba007a17
cyxkat157 <svhbatb0n@gmail.com>
2024-10-09 14:18:31 +0800

    password

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,28 @@
-import Documents from "../components/Documents";
-import Menu from "../components/Menu";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 const Profile = () => {
+  const router = useRouter();
+  const now = new Date();
+  const day = now.getDate();
+
+  const check = () => {
+    const nuuts = document.getElementById("nuuts").value;
+    if (nuuts == day) {
+      router.push(`/day/${day}`);
+    } else {
+      alert("Wrong password!");
+    }
+  };
+
   return (
-    <div className="bg-white h-full flex justify-center items-center">
-      <h1>
-        Энэхүү веб сайтыг хийсэн хүн шоронд орсон тул түр хугацаагаар хаагдсан
-      </h1>
-      {/* <Documents /> */}
-      {/* <Menu /> */}
+    <div className="bg-white h-full flex flex-col items-center justify-center">
+      <h1>Нууц үгээ худалдаж аваад ороорой</h1>
+      <input className="border-2  w-24" placeholder="Password" id="nuuts" />
+      <button className="border-2 w-24" onClick={check}>
+        Click
+      </button>
     </div>
   );
 };
